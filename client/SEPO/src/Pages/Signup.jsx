e364ba85f0ac6ce5f1cc511191d1c67ae0560d7c
Evan F <evanfd05@gmail.com>
2024-02-08 18:28:36 -0700

    new login and signup

--- client/SEPO/src/Pages/Signup.jsx
+++ client/SEPO/src/Pages/Signup.jsx
@@ -47,69 +47,70 @@ console.log(data);
   };
 
   return (
-    <main className="d-flex justify-content-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card shadow-sm">
-          <div className="card-body">
-            <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-              <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
-                Something went wrong with your signup!
-              </Alert>
+    <div className="d-flex justify-content-center mt-5"> {/* Add margin-top here */}
+        <div className="col-12 col-md-6">
+            <div className="card shadow-sm">
+                <div className="card-body">
+                    <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+                        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
+                            Something went wrong with your signup!
+                        </Alert>
 
-              <Form.Group className="mb-3">
-                <h1>Sign Up:</h1>
-                <Form.Label htmlFor="username">Username</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Username"
-                  name="username"
-                  onChange={handleInputChange}
-                  value={userFormData.username}
-                  required
-                />
-                <Form.Control.Feedback type="invalid">Username is required!</Form.Control.Feedback>
-              </Form.Group>
+                        <Form.Group className="mb-3">
+                            <h1>Sign Up:</h1>
+                            <Form.Label htmlFor="username">Username</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Username"
+                                name="username"
+                                onChange={handleInputChange}
+                                value={userFormData.username}
+                                required
+                            />
+                            <Form.Control.Feedback type="invalid">Username is required!</Form.Control.Feedback>
+                        </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="email">Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  onChange={handleInputChange}
-                  value={userFormData.email}
-                  required
-                />
-                <Form.Control.Feedback type="invalid">Email is required!</Form.Control.Feedback>
-              </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor="email">Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                name="email"
+                                onChange={handleInputChange}
+                                value={userFormData.email}
+                                required
+                            />
+                            <Form.Control.Feedback type="invalid">Email is required!</Form.Control.Feedback>
+                        </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Label htmlFor="password">Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  onChange={handleInputChange}
-                  value={userFormData.password}
-                  required
-                />
-                <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
-              </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor="password">Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                onChange={handleInputChange}
+                                value={userFormData.password}
+                                required
+                            />
+                            <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
+                        </Form.Group>
 
-              <Button
-                disabled={!(userFormData.username && userFormData.email && userFormData.password)}
-                type="submit"
-                variant="primary"
-                className="btn-block"
-              >
-                Submit
-              </Button>
-            </Form>
-          </div>
+                        <Button
+                            disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+                            type="submit"
+                            variant="primary"
+                            className="btn-block"
+                        >
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
         </div>
-      </div>
-    </main>
-  );
+    </div>
+);
 };
+
 
 export default SignupForm;
