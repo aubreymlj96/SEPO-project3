f2d75a4aeb07504e60441e062affe836ae952b37
aubrey johnson <aubreymlj96@gmail.com>
2024-02-14 15:49:02 -0500

    added footer back and updated create event form

--- client/src/Pages/CreateEvent.jsx
+++ client/src/Pages/CreateEvent.jsx
@@ -51,56 +51,48 @@ const CreateEvent = ({ onCreateEvent }) => {
   };
 
   return (
-    <div className="form-container">
-      <h1>Create an Event</h1>
-      <form className="event-form" onSubmit={handleSubmit}>
-        {/* Sport Selection */}
-        <div className="form-group">
-          <label htmlFor="sport">Choose a sport:</label>
-          <select id="sport" value={sport} onChange={(e) => setSport(e.target.value)}>
-            <option value="">Select a sport</option>
-            <option value="Football">Football</option>
-            <option value="Baseball">Baseball</option>
-            <option value="Basketball">Basketball</option>
-          </select>
+<div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <h1>Create an Event</h1>
+          <form onSubmit={handleSubmit}>
+            {/* Sport Selection */}
+            <div className="form-group">
+              <label htmlFor="sport">Choose a sport:</label>
+              <select className="form-control" id="sport" value={sport} onChange={(e) => setSport(e.target.value)}>
+                <option value="">Select a sport</option>
+                <option value="Football">Football</option>
+                <option value="Baseball">Baseball</option>
+                <option value="Basketball">Basketball</option>
+              </select>
+            </div>
+            {/* Date and Time Input */}
+            <div className="form-group">
+              <label htmlFor="dateTime">Date and Time:</label>
+              <input className="form-control" id="dateTime" type="datetime-local" value={DateTime} onChange={(e) => setDateTime(e.target.value)} />
+            </div>
+            {/* Location Input */}
+            <div className="form-group">
+              <label htmlFor="location">Location:</label>
+              <input className="form-control" id="location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="players">Players:</label>
+              <input className="form-control" id="players" type="number" value={players} onChange={(e) => setPlayers(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Event Name:</label>
+              <input className="form-control" id="eventName" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="eventText">Event Text:</label>
+              <input className="form-control" id="eventText" type="text" value={eventText} onChange={(e) => setEventText(e.target.value)} />
+            </div>
+            {/* Submit Button */}
+            <button className="btn btn-primary mt-3" type="submit">Create Event</button>
+          </form>
         </div>
-  
-        {/* Date and Time Input */}
-        <div className="form-group">
-          <label htmlFor="dateTime">Date and Time:</label>
-          <input id="dateTime" type="datetime-local" value={DateTime} onChange={(e) => setDateTime(e.target.value)} />
-        </div>
-  
-        {/* Location Input */}
-        <div className="form-group">
-          <label htmlFor="location">Location:</label>
-          <input id="location" type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
-        </div>
-
-        {/* Max People Input */}
-        {/* <div className="form-group">
-          <label htmlFor="maxPeople">Maximum People:</label>
-          <input id="maxPeople" type="number" value={maxPeople} onChange={(e) => setMaxPeople(e.target.value)} />
-        </div> */}
-
-        <div className="form-group">
-          <label htmlFor="players">players:</label>
-          <input id="players" type="number" value={players} onChange={(e) => setPlayers(e.target.value)} />
-        </div>
-  
-        <div className="form-group">
-          <label htmlFor="name">name:</label>
-          <input id="eventName" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="eventText">Text:</label>
-          <input id="eventText" type="text" value={eventText} onChange={(e) => setEventText(e.target.value)} />
-        </div>
-
-        {/* Submit Button */}
-        <button type="submit">Create Event</button>
-      </form>
+      </div>
     </div>
   );
 };
